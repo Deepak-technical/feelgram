502a71e3470d10ab7bd737eaf3f975c1871bf439
Deepak-technical <deepakakash156@gmail.com>
2023-11-23 16:35:16 +0530

    code added

--- src/components/shared/UserCard.tsx
+++ src/components/shared/UserCard.tsx
@@ -9,17 +9,23 @@ type UserCardProps = {
 
 const UserCard = ({ user }: UserCardProps) => {
   return (
-    <Link to={`/profile/${user.$id}`} className="user-card">
+    <Link to={`/profile/${user.$id}`} className="flex flex-row justify-between md:user-card">
       <img
         src={user.imageUrl || "/assets/icons/profile-placeholder.svg"}
         alt="creator"
         className="rounded-full w-14 h-14"
       />
 
-      <div className="flex-center flex-col gap-1">
-        <p className="base-medium text-light-1 text-center line-clamp-1">
-          {user.name}
+      <div className="flex flex-col justify-center items-start md:flex-col gap-1">
+        <p className="base-medium text-light-1 flex flex-row items-center gap-x-2  text-center line-clamp-1">
+          {user.name} {user.emailVerified ? (
+                <p>
+                <img src='/assets/icons/tick.png' height={16} width={16} /></p>
+              ) : (
+                <></>
+              )}
         </p>
+       
         <p className="small-regular text-light-3 text-center line-clamp-1">
           @{user.username}
         </p>
