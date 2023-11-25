c6c76e08aae810e740f8d7c55dd7a3afab152d13
Deepak-technical <deepakakash156@gmail.com>
2023-11-25 16:49:23 +0530

    code added

--- src/_auth/forms/SignUpForm.tsx
+++ src/_auth/forms/SignUpForm.tsx
@@ -69,7 +69,7 @@ const SignUpForm = () => {
     <>
       <Form {...form}>
         <div className="sm:w-420 flex-center flex-col ">
-          <img src="/public/assets/images/finallogo.png" alt="" width={280} />
+          <img src="/assets/images/finallogo.png" alt="" width={280} />
           <h2 className="h3-bold md:h2-bold pt-5 sm:pt-8">
             Create an new account
           </h2>
